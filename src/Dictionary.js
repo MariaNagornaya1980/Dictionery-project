2c1543e8dbd520515c86610978e2eda914cb11a3
MariaNagornaya1980 <mariawright205@gmail.com>
2024-10-30 15:43:00 -0700

    added css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,35 +1,54 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
+  let [photos, setPhotos] = useState(null);
+
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "LfMyR5Osm97WbJE4iVJo9qs4MpEaGkhmRLeo2kwiEGoljCnkNts6hGV4";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   function load() {
     setLoaded(true);
     search();
   }
+
   if (loaded) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>What word would you like to look up?</h1>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -38,14 +57,15 @@ export default function Dictionary(props) {
             />
           </form>
           <div className="hint">
-            suggested words: sunset, wine, love, yoga, forest...
+            suggested words: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
     load();
-    return "Loading...";
+    return "Loading";
   }
 }
